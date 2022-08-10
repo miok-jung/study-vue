b9efbfda5218b6789b2f106d324e3ba31596a394
miok-jung <miok.dev@gmail.com>
2022-08-10 16:25:53 +0900

    fix: vuetify/textfield에 대한 오류 수정 및 추가 공부

--- src/views/vuetify/TextField.vue
+++ src/views/vuetify/TextField.vue
@@ -1,28 +1,26 @@
-<template>
-  <!-- <TextFieldVue /> -->
+<!-- <template>
+  <TextFieldVue />
   <TextField ref="inputRef" :text="text" @customInput="(e) => (text = e)">
   </TextField>
   <input v-model="text" />
   <v-btn
     :style="[
       {
-        color: test ? 'red' : 'black',
+        color: text ? 'red' : 'black',
       },
     ]"
     @click="inputRef?.hide()"
   >
     안보여
   </v-btn>
-  <!-- LINK https://pusha.tistory.com/entry/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-%EB%AC%BC%EC%9D%8C%ED%91%9C-%EC%84%A0%ED%83%9D%EC%A0%81-%EC%97%B0%EC%82%B0%EC%9E%90-Optional-chaining -->
-  <!-- NOTE 데이터가 undefined가 나올 때 에러를 방지해주는데 거의 문제가 발생하지 않지만, 많이 쓰인다. -->
   <v-btn @click="inputRef?.show()"> 보여 </v-btn>
-</template>
+</template> -->
 
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 // import TextFieldVue from "@/components/vuetify/TextField.vue";
 // import TextField from "@/components/TextField.vue";
-import { onMounted, ref, CSSProperties } from "vue";
-// import { cpuUsage } from "process";
+// import { onMounted, ref, CSSProperties } from "vue";
+import { onMounted, ref } from "vue";
 const text = ref("fdsa");
 interface ICustomInput {
   show?: () => void;
@@ -31,10 +29,10 @@ interface ICustomInput {
 
 // fdsa
 
-const redbuto: CSSProperties = {
-  border: "1px solid black",
-};
-// const black;
+// const redbuto: CSSProperties = {
+//   border: "1px solid black",
+// };
+// // const black;
 
 function temp1(test: string, test2?: string) {
   if (test2) console.log("test2가 있다");
@@ -50,4 +48,13 @@ const inputRef = ref<ICustomInput>();
 onMounted(() => {
   console.log(inputRef.value);
 });
+</script> -->
+<template>
+  <TextFieldVue />
+</template>
+
+<script setup lang="ts">
+import TextFieldVue from "@/components/vuetify/TextField.vue";
 </script>
+
+<style scoped></style>
