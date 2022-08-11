d17b1c93ee85d4a4966a87f8de4a268c50fb9006
miok-jung <miok.dev@gmail.com>
2022-08-11 10:46:54 +0900

    fix: header 고정, 불필요 내용 제거

--- src/components/NavigationHeader.vue
+++ src/components/NavigationHeader.vue
@@ -3,26 +3,17 @@
     <router-link to="/">NAMES</router-link>
     <router-link to="/vue">Vue</router-link>
     <router-link to="/vuetify">Vuetify</router-link>
-    <p>{{ printCoord({ x: 6, y: 3 }) }}</p>
   </header>
 </template>
 
-<script setup lang="ts">
-const printCoord = (pt: { x: number; y: number }) => {
-  // console.log("X: ", pt.x);
-  // console.log("Y: ", pt.y);
-  return `${pt.x}, ${pt.y}`;
-};
-// printCoord({ x: 3, y: "string" }); // 나오기는 하나 잘못된 형식이라고 나옴
-// printCoord({ x: 3, y: 8 });
-</script>
-
 <style lang="scss" scoped>
 header {
-  display: flex;
-  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background: $color2;
   height: 2rem;
-  background: $color4;
   a {
     padding: 0 0.5rem;
     line-height: 2rem;
