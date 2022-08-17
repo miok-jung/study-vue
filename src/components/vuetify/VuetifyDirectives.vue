e89b21858220d91dc8cb2241f0a78b91f2e6c2d7
miok-jung <miok.dev@gmail.com>
2022-08-17 17:55:21 +0900

    study: study to directives

---
+++ src/components/vuetify/VuetifyDirectives.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrap">
+    <h2>Directive</h2>
+    <v-card
+      class="mx-auto"
+      width="256"
+      height="256"
+      rounded="xl"
+      v-click-outside="onClickOutside"
+      @click="active = true"
+    >
+      <div class="d-flex justify-center align-center fill-height">
+        {{ active ? "Click Outside" : "Click Me" }}
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const active = ref<boolean>(false);
+const onClickOutside = () => {
+  active.value = false;
+};
+</script>
+
+<style scoped></style>
--- src/views/vuetify/GridDialog.vue
+++ src/views/vuetify/GridDialog.vue
@@ -1,7 +1,9 @@
 <template>
   <GridDialog />
+  <VuetifyDirectives />
 </template>
 
 <script setup lang="ts">
 import GridDialog from "@/components/vuetify/GridDialog.vue";
+import VuetifyDirectives from "@/components/vuetify/VuetifyDirectives.vue";
 </script>
