34d56bdeb3d9ad65e8251d06be45f06e91a9f173
miok-jung <miok.dev@gmail.com>
2022-08-17 11:05:43 +0900

    study : Study to Table of vuetify

--- src/views/vuetify/SystemBar.vue
+++ src/views/vuetify/SystemBar.vue
@@ -1,7 +1,9 @@
 <template>
   <SystemBarVue />
+  <VuetifyTable />
 </template>
 
 <script setup lang="ts">
 import SystemBarVue from "@/components/vuetify/SystemBar.vue";
+import VuetifyTable from "@/components/vuetify/VuetifyTable.vue";
 </script>
