6c9e01589f599e4095a00576ac978534ddbb0e7e
miok-jung <miok.dev@gmail.com>
2022-08-26 09:35:42 +0900

    feat: Change to Main page

---
+++ src/views/main-home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrap">
+    <h1>Main Home</h1>
+    <p>This is Study Vue HomePages</p>
+  </div>
+</template>
+<style scoped lang="scss">
+.wrap {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
